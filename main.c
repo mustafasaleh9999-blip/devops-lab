2e46ce45dc903d0f9cffb44e30aae77210721b8f
mustafasaleh9999-blip <mustafasaleh9999@gmail.com>
2026-06-12 13:24:31 +0300

    first See plus plus projects.

--- main.c
+++
@@ -1,7 +0,0 @@
-#include <stdio.h>
-
-int main()
-{
-    printf("CI/CD Lab\n");
-    return 0;
-}
---
+++ main.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+
+int main() {
+    std::cout << "Hello, World! Mustafa" << std::endl;
+    return 0;
+}
